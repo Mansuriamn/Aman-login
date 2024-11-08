a04734cfc9cf314a695d41ea68a3c018d0c710f0
Aman Mansuri <162823789+Mansuriamn@users.noreply.github.com>
2024-11-08 21:34:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+     origin: 'http://localhost:5000' // Replace with your frontend URL
+   }));
 app.use(express.json());
 app.use(express.static(path.join(_dirname, "/frontend/dist")));
 
